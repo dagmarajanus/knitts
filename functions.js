d4ab0555aa983e635a6da76c6aa2bb9bf14646bc
dagmarajanus <daga0919@o2.pl>
2022-11-08 18:12:54 +0100

    commit

--- functions.js
+++ functions.js
@@ -1,7 +1,20 @@
-$(window).scroll(function() {
-    if ($(window).scrollTop()) {
+// $(window).scroll(function() {
+
+//     if ($(window).scrollTop()) {
+//         $("nav").addClass("bluenav");
+
+//     } else {
+//         $("nav").
+//     }
+// });
+
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         $("nav").addClass("bluenav");
     } else {
         $("nav").removeClass("bluenav");
     }
-});
+}
